98c3e792c442a5c406fbf9c826c8945ed4a15f21
Hicham SADDEK <hicham@north-star.network>
2024-09-23 22:45:50 +0100

    Adding sentry session replay.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,21 +1,32 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import * as Sentry from "@sentry/vue";
+import {createApp, DefineComponent, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+    setup({el, App, props, plugin}) {
+        let app = createApp({render: () => h(App, props)});
+        Sentry.init({
+            app,
+            dsn: import.meta.env.VITE_SENTRY_DSN,
+            integrations: [
+                Sentry.replayIntegration(),
+            ],
+            // Session Replay
+            replaysSessionSampleRate: 0.1,
+            replaysOnErrorSampleRate: 1.0,
+        });
+        app.use(plugin);
+        app.use(ZiggyVue);
+        app.mount(el);
     },
     progress: {
         color: '#4B5563',
